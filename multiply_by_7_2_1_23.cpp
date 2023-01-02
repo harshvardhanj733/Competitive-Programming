7ad4a59e12e84562e0aad8fcfd2869e41b0910fc
harshvardhanj733 <harshvardhanj733@gmail.com>
2023-01-02 15:01:15 +0530

    Made 1st iteration of Multiply by 7

--- multiply_by_7_2_1_23.cpp
+++ multiply_by_7_2_1_23.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <string>
+#include <cmath>
 using namespace std;
 
 int main(){
     //entering n inputs;
-    int n;
+    int n; string arr[500];
+    cin>>n;
+    for(int i = 0; i<n; i++){
+        long num; int m;
+        cin>>num>>m;
+        string num1 = to_string(num);
+        int length = num1.length();
+        long number = num;
+        int arr2[10];
+        for(int j = length-1; j>=0; j--){
+            int digit = number/pow(10, j);
+            number = number % (int)pow(10, j);
+            digit = pow(digit, m);
+            digit = digit%10;
+            arr2[j] = digit;
+        }
+        long fin_num = 0;
+        for(int j = length-1; j>=0; j--){
+            fin_num += arr2[j]*pow(10, j);
+        }
+        if(fin_num % 7 == 0){
+            arr[i] = "YES";
+        }
+        else{
+            arr[i] = "NO";
+        }
+    }
+    for(int i = 0; i<n; i++){
+        cout<<arr[i]<<endl;
+    }
 return 0;
 }
